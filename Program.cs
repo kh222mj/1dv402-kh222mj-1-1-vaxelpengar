819554591f755251f485697b5f8934bb4258d3dc
kh222mj <kh222mj@lnu.se>
2014-09-09 22:56:43 +0200

    lagt till ny fil, program.cs, påbörjat lab-vaxelpengar

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //deklaration av variablar
+            double total;
+            int belopp;
+
+            // try-cath
+
+            //läs in värden
+            Console.Write("Ange totalsumma:");
+            total = double.Parse(Console.ReadLine());
+
+            Console.Write("Ange erhållet belopp:");
+            belopp = int.Parse(Console.ReadLine());
+
+            //Presentera Kvitto
+            Console.WriteLine("KVITTO");
+            
+            
+
+            
+            
+        }
+    }
+}
